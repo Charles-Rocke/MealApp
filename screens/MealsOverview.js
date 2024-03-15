d9f2a6d37a48fd091469a7498edb22f6b5fd7829
Spook <spook.devv@gmail.com>
2024-03-15 09:52:01 -0400

    Displaying Meals

--- screens/MealsOverview.js
+++ screens/MealsOverview.js
@@ -1,15 +1,31 @@
-import { View, Text, StyleSheet } from "react-native";
+import { View, FlatList, StyleSheet, Text } from "react-native";
 // import { useRoute } from "@react-navigation/native"; alternative to what has been done
 import { MEALS } from "../data/dummyData";
+import MealItem from "../components/MealItem";
 
 function MealsOverview({ route }) {
   // const route = useRoute(); alternative to what is done below
   // route.params.categoryId; alternative to what is done below
   const categoryId = route.params.categoryId;
+  const displayedMeals = MEALS.filter((mealItem) => {
+    return mealItem.categoryIds.indexOf(categoryId) >= 0;
+  });
+
+  function renderMealItem(itemData) {
+    return (
+      <View>
+        <MealItem title={itemData.item.title}></MealItem>
+      </View>
+    );
+  }
 
   return (
     <View style={styles.container}>
-      <Text>Meals Overview Screen -- {categoryId}</Text>
+      <FlatList
+        data={displayedMeals}
+        keyExtractor={(item) => item.id}
+        renderItem={renderMealItem}
+      />
     </View>
   );
 }
